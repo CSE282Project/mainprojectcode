b13c9a1c9324f8b3dc3eed08d226be2c6c449170
Arjun <arjunc12@gmail.com>
2015-02-18 23:22:46 -0800

    added documentation, fixed a couple bugs

--- ctcf_matchings.py
+++ ctcf_matchings.py
@@ -1,36 +1,56 @@
 from sys import argv
 
 class Matching:
+    '''
+    A matching object is a collection of vertex-disjoint edges
+    '''
     def __init__(self, edges = None):
         if edges == None:
-            self.edges = []
+            edges = set()
         self.vertices = set()
-        self.add_edges(edge)
+        self.edges = set()
+        self.add_edges(edges)
     
     def get_edges(self):
         return self.edges
     
     def add_edge(self, edge):
         u, v = edge
+        assert u > 0
+        assert v < 0
         
         assert u not in self.vertices
         assert v not in self.vertices
         
         self.vertices.update(edge)
-        self.edges.append(edge)
+        self.edges.add(edge)
     
     def add_edges(self, edges):
         for edge in edges:
             self.add_edge(edge)
         
     def add_matching(self, matching):
-        self.add_edges(matching.get_edges)
+        '''
+        merge with other matching
+        '''
+        if matching != None:
+            self.add_edges(matching.get_edges())
     
-    def get_score(self):
+    def get_weight(self):
+        '''
+        weight a matching as simply the average distance between vertices
+        '''
+        if len(self.edges) == 0:
+            '''
+            if matching is empty just return 0, 0
+            this makes it easy when combining the scores of multiple matchings
+            admittedly this may not be 100% robust but for now i think it's fine
+            '''
+            return 0, 0
         total = 0.0
         n = len(self.edges)
         for u, v in self.edges:
-            total += abs(abs(u) - abs(v))
+            total += abs(u - abs(v))
         return total / n, n
         
     def __add__(self, other):
@@ -54,11 +74,16 @@ def get_vertices(genome, motifs, k):
     revc_nodes = []
     
     for i in range(len(genome) - k + 1):
+        '''
+        use 1-based indexing, so that positive/negative values delineate motifs from
+        reverse complements
+        '''
+        pos = i + 1
         kmer = genome[i:i + k]
         if kmer in motifs:
-            motif_nodes.append(i)
+            motif_nodes.append(pos)
         elif kmer in revc_nodes:
-            revc_nodes.append(-1)
+            revc_nodes.append(-pos)
     
     '''
     we know the graph is bipartite and semi-complete
@@ -68,6 +93,33 @@ def get_vertices(genome, motifs, k):
     return motif_nodes, revc_nodes
 
 def matching_helper(motif_nodes, revc_nodes, k, start, end, sub_matchings):
+    '''
+    helper function to find the minimum-weight maximal on a certain subsection of the 
+    genome
+    
+    Args:
+        motif_nodes: the locations of CTCF binding motifs
+        
+        revc_nodes: the locations of reverse complements to CTCF binding motifs
+        
+        k: length of the motifs
+        
+        start, end: the endpoints of the region on which we should find our matching
+        ensures that whatever matching the callee returns does not cross with the caller's
+        matching
+        
+        sub_matchings: dictionary of minimally-weighted matchings on previously computed
+        intervals, to avoid repeated computation
+        
+    Returns:
+        The non-crossing maximal matching that minimizes the average distance between 
+        nodes in the matching among the space of all non-crossing maximal matchings on 
+        the interval.  Returns an empty matching if there are no edges within this
+        interval
+    '''
+    assert start <= 0
+    assert end < len(genome) - k
+    
     if (start, end) in sub_matchings:
         return sub_matchings[(start, end)]
     
@@ -75,31 +127,75 @@ def matching_helper(motif_nodes, revc_nodes, k, start, end, sub_matchings):
     motifs = filter(inbounds, motif_nodes)
     reverses = filter(inbounds, revc_nodes)
     
-    best_matching = None
+    '''
+    instead of initializing best_matching to None we initialize it to an empty matching
+    if there are no edges within the interval, then an empty matching will be returned
+    which is what we want
+    '''
+    best_matching = Matching()
     best_score = float("inf")
     
     for motif in motifs:
+        assert motif > 0
         for revc in revc_nodes:
-            left = matching_helper(motif_nodes, revc_nodes, k, 0, start - 1, sub_matchings)
-            right = matching_helper(motif_nodes, revc_nodes, k, end + 1, len(genome), sub_matchings)
-            mid = matching_helper(motif_nodes, revc_nodes, k, start + k, end - 1, sub_matchings)
+            assert revc < 0
+            '''
+            adding edge (motif, revc) to the matching, then finding the best 
+            non-crossing matching that includes this edge
             
-            left_mean, left_n = left.get_score()
-            right_mean, right_n = left.get_score()
-            mid_mean, mid_n = left.get_score()
+            by (greedily) adding whatever non-crossing edges possible we assure that our
+            matching is maximally non-crossing.  We take the best-weighted matching 
+            among maximal non-crossing matchings.  We use memoization to avoid repated
+            computation.
+            '''
             
+            first, last = sorted([motif, abs(revc)])
+            
+            # get best matching to the left of the edge, but within the interval
+            left = matching_helper(motif_nodes, revc_nodes, k, start, first - k, sub_matchings)
+            
+            # get best matching to the right of the edge, 
+            right = matching_helper(motif_nodes, revc_nodes, k, last + k, end, sub_matchings)
+            
+            # get best matching within the loop formed by this edge
+            mid = matching_helper(motif_nodes, revc_nodes, k, first + k, last - k, sub_matchings)
+            
+            left_mean, left_n = left.get_weight()
+            right_mean, right_n = left.get_weight()
+            mid_mean, mid_n = left.get_weight()
+            
+            # get the weighted sum of the optimal sub-matchings that do not cross 
+            # (motif, revc)
             total = (left_mean * left_n) + (right_mean * right_n) + (mid_mean * mid_n)
             dist = abs(motif - abs(revc))
             total += dist
             n = left_n + right_n + mid_n + 1
             score = float(total) / n
+           
             if score < best_score:
                 best_matching = left + right + mid
                 best_matching.add_edge((motif, revc))
+                best_score = score
                 
     sub_matchings[(start, end)] = best_matching
     return best_matching
     
 def maximal_matching(genome, motifs, k):
+    # generate the graph
     motif_nodes, revc_nodes = get_vertices(genome, motifs, k)
-    return matching_helper(motif_nodes, revc_nodes, k, 0, len(genome) - 1, {})
+    
+    '''
+    call the helper function, starting with an empty dictionary and setting the 
+    endpoints to be the entire genome
+    1-based indexing of nodes, so initial endpoints are (1, |G| - k)
+    have not computed anything, so sub_matchings is empty, but since dictionaries are
+    pointers we can pass by reference, and when it is updated by the callee during
+    recursive calls the changes will be reflected in the caller's stack frame
+    '''
+    return matching_helper(motif_nodes, revc_nodes, k, 1, len(genome) - k, {})
+    
+if __name__ == '__main__':
+    genome = TTGAACTGAGCGAAGAAAGATCGCAGACTGTCAA
+    motifs = [TTGA, GCGA]
+    k = 4
+    print maximal_matching(genome, motifs, k)
