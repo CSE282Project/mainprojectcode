d1e19052fb985859111fc9b0a518a74355259936
Samuel Roth <sjroth@Samuels-MacBook-Pro-4.local>
2015-03-02 01:21:45 -0800

    updated version of random sequence genrator

--- GenerateRandomSeqWithMotifs.py
+++ GenerateRandomSeqWithMotifs.py
@@ -8,6 +8,17 @@ import random
 def generate_genome(L):
     genome = ''.join([random.choice(['A','T','C','G']) for i in range(L)])
     return genome
+'''
+Create a function that generates the reverse complement of a given sequence.
+Input: sequence
+Output: reverse complement
+'''
+def rev_comp(sequence):
+    out = ''
+    comp_dict = {'A':'T','T':'A','C':'G','G':'C'}
+    for char in sequence:
+        out += comp_dict[char]
+    return out[::-1]
 
 #Create a function that checks if the coordinates for two overlaps.
 #Input: i, j
@@ -17,6 +28,35 @@ def is_overlap(i,j):
     if i[0] >= j[0] and i[0] <= j[1]: overlap = True
     if i[1] >= j[0] and i[1] <= j[1]: overlap = True
     return overlap
+
+'''
+Create a function that finds a new location for inserting a motif.
+WARNING: Be sure that you are inserting a motif where there is room. Otherwise,
+this function will loop forever.
+Input: motif_locations, genome_length, motif_length
+'''
+def find_insertion_location(motif_locations,genome_length,motif_length):
+
+    #Keep iterating until you find a new location.
+    j = 0
+    not_found_location = True
+    while not_found_location:
+
+        #Pick an initial random location.
+        rand_location = random.randrange(genome_length-motif_length)
+        motif_location = (rand_location,rand_location+motif_length)
+
+        #Iterate through the locations that have been used.
+        used = False
+        i = 0
+        while i < len(motif_locations) and not used:
+            used_location = motif_locations[i]
+            if is_overlap(used_location,motif_location): used = True
+            i += 1
+        if not used:
+            not_found_location = False
+
+    return motif_location
 
 #Create a function to insert n motifs into a random sequence of length L.
 #Input: n, L, motifs
@@ -30,20 +70,29 @@ def generate_genome_with_motifs(n,L,motifs):
     motif_len = len(motifs[0])
 
     #Insert n motifs. Make sure to insert the motifs in distinct locations.
+    #Keep track of which motifs are in the genome.
     motif_locations = []
+    print genome
+    unused_motifs = set(motifs)
     for i in range(n):
-        motif = random.choice(motifs)
-        rand_location = random.randrange(L-motif_len)
-        motif_location = (rand_location,rand_location+motif_len)
-        not_used = True
-        j = 0
-        while j < len(motif_locations) and not_used:
-            used_location = motif_locations[j]
-            if is_overlap(used_location,motif_location): not_used = False
-            j += 1
+
+        #If there is an unused motif, pick it. Otherwise randomly choose a motif.
+        if unused_motifs:
+            motif = unused_motifs.pop()
+        else:
+            motif = random.choice(motif)
+        rev_comp_motif = rev_comp(motif)
+
+        #Find locations for the insertions.
+        motif_location = find_insertion_location(motif_locations,L,motif_len)
         motif_locations.append(motif_location)
-        if not_used:
-            genome = genome[:motif_location[0]]+motif+genome[motif_location[1]:]
+        rev_comp_location = find_insertion_location(motif_locations,L,motif_len)
+        motif_locations.append(rev_comp_location)
+        #Insert the motif into the genome.
+        genome = genome[:motif_location[0]]+motif+genome[motif_location[1]:]
+        genome = genome[:rev_comp_location[0]]+rev_comp_motif+\
+                 genome[rev_comp_location[1]:]
+
     return genome
 
 '''
