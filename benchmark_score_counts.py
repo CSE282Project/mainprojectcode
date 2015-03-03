8db13198fb64f409ce471a2ac4bb01dc8f37cdd1
Benjamin P. Kellman <bkellman@ucsd.edu>
2015-03-02 18:30:58 -0800

    rough draft of 3d plotting benchmark code

--- benchmark_score_counts.py
+++ benchmark_score_counts.py
@@ -18,7 +18,7 @@ def individual_test(f,genome,motifs,k):
 		weight = matching.get_weight()[0]
 		recursiveCount = 0
 		count = 0
-	elif f=='Dynamic':
+	elif f=='Dynamic_Programming':
 		from ctcf_matchings import recursiveCount
 		from ctcf_matchings import count
 		match.initCounter()  # initialize counters
@@ -40,16 +40,54 @@ def individual_test(f,genome,motifs,k):
 
 	return recursiveCount,count,weight
 
+def plotin3d(fig,sub,x,y,Z,xlab,ylab,title):
+	from mpl_toolkits.mplot3d import Axes3D
+	from matplotlib import cm
+	from matplotlib.ticker import LinearLocator, FormatStrFormatter
+	import matplotlib.pyplot as plt
+	import numpy as np
+
+	#fig = plt.figure()
+	fig.add_subplot(sub)
+	ax = fig.gca(projection='3d')
+	#X = np.arange(-5, 5, 0.25)
+	#Y = np.arange(-5, 5, 0.25)
+	X = np.array(x)
+	Y = np.array(y)
+	X, Y = np.meshgrid(X, Y)
+	#R = np.sqrt(X**2 + Y**2)
+	#Z = np.sin(R)
+	surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+	        linewidth=0, antialiased=False)
+	ax.set_zlim(np.min(Z)-abs(np.min(Z)), np.max(Z)+abs(np.max(Z)))
+
+	ax.zaxis.set_major_locator(LinearLocator(10))
+	ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+	fig.colorbar(surf, shrink=0.5, aspect=5)
+
+	ax.set_xlabel(xlab)
+	ax.set_ylabel(ylab)
+	ax.set_title(title)
+
+	#plt.show()
+
 
 if __name__ == '__main__':
-#def main():
+	test = True
+	d2 = False
+
 	# Which algorithms
-	algorithms = ['Greedy','Dynamic']#,'Complete']
+	algorithms = ['Greedy','Dynamic_Programming']#,'Complete']
 	# Genome lengths
 	#L   = [round(10**i) for i in range(3,4,.5)]
-	L = [150,200,300,400,500,600]
-	# n replicates of m motifs of length k
-	m = [3,4,5,6,10,12,14,16]#,500,1000,5000]
+	if test:
+		L = [150,200]
+		m = [3,4]
+	else:
+		L = [150,200,300,400,500,550,600]
+		# n replicates of m motifs of length k
+		m = [3,4,5,6,10,12,14,16]#,500,1000,5000]
 	n = m
 	k = 5
 
@@ -73,12 +111,12 @@ if __name__ == '__main__':
 				elapsed[f,i,j] = after-before
 
 
-d2 = True
 
 if(d2):
-	fixL = 3
+	fixL = 5
 	fixm = 7
 	plt.figure(1)
+	plt.title('Count, Score and Runtime Analysis')
 	# recursive counts vs L
 	mat = rec_count
 	# red dashes, blue dashes and green dashes
@@ -86,6 +124,7 @@ if(d2):
 	plt.plot(L,mat[0,:,fixm], 'r--', L, mat[1,:,fixm],'b--')
 	plt.xlabel('Length of Genome')
 	plt.ylabel('Recursive Counts')
+	plt.ylim(-100,100000)
 	# recursive counts vs m
 	# red dashes, blue dashes and green dashes
 	plt.subplot(422)
@@ -100,6 +139,7 @@ if(d2):
 	plt.plot(L,mat[0,:,fixm], 'r--', L, mat[1,:,fixm],'b--')
 	plt.xlabel('Length of Genome')
 	plt.ylabel('T(n)')
+	plt.ylim(-100,6000000)
 	# recursive counts vs m
 	# red dashes, blue dashes and green dashes
 	plt.subplot(424)
@@ -126,16 +166,77 @@ if(d2):
 	# red dashes, blue dashes and green dashes
 	plt.subplot(427)
 	plt.plot(L,mat[0,:,fixm], 'r--', L, mat[1,:,fixm],'b--')
-	plt.xlabel('Length of Genome')
+	plt.xlabel('Length of Genome (L) '+'(fixed m='+str(m[fixm])+')')
 	plt.ylabel('Time Elapsed (sec)')
+	plt.ylim(-5,50)
 	# recursive counts vs m
 	# red dashes, blue dashes and green dashes
 	plt.subplot(428)
 	plt.plot(m,mat[0,fixL,:], 'r--', m, mat[1,fixL,:],'b--')
-	plt.xlabel('Number of Forward CTCF Binding Motifs')
+	plt.xlabel('Number of Forward CTCF Binding Motifs (m)'+'(fixed L='+str(L[fixL])+')')
 	plt.ylabel('Time Elapsed (sec)')
+	plt.ylim(-5,50)
 
 	plt.show()
 else:
+	x = L
+	y = m
+	fig = plt.figure(1)
+	plt.title('Count, Score and Runtime Analysis')
+	# recursive counts vs L vs m, dp
+	mat = rec_count
+	Z = mat[1,:,:]
+	sub  = '321'
+	xlab = 'Length of Genome'
+	ylab = 'Number of Forward CTCF Binding Motifs'
+	zlab = 'Recursive Counts'
+	title= 'Dynamic Programming Approach: Recursive Counts'
+	plotin3d(fig,sub,x,y,Z,xlab,ylab,title)
+	#  counts vs L vs m, dp
+	mat = T_n
+	Z = mat[1,:,:]
+	sub  = '322'
+	xlab = 'Length of Genome'
+	ylab = 'Number of Forward CTCF Binding Motifs'
+	zlab = 'Counts, T(n)'
+	title= 'Dynamic Programming Approach: Counts, T(n)'
+	plotin3d(fig,sub,x,y,Z,xlab,ylab,title)
+	#  score vs L vs m, dp
+	mat = weight
+	Z = mat[1,:,:]
+	sub  = '323'
+	xlab = 'Length of Genome'
+	ylab = 'Number of Forward CTCF Binding Motifs'
+	zlab = 'Score'
+	title= 'Dynamic Programming Approach: Score'
+	plotin3d(fig,sub,x,y,Z,xlab,ylab,title)
+	#  score vs L vs m, greedy
+	Z = mat[0,:,:]
+	sub  = '324'
+	xlab = 'Length of Genome'
+	ylab = 'Number of Forward CTCF Binding Motifs'
+	zlab = 'Score'
+	title= 'Greedy Approach: Score'
+	plotin3d(fig,sub,x,y,Z,xlab,ylab,title)
+	#  time vs L vs m, dp
+	mat = elapsed
+	Z = mat[1,:,:]
+	sub  = '325'
+	xlab = 'Length of Genome'
+	ylab = 'Number of Forward CTCF Binding Motifs'
+	zlab = 'Time Elapsed (sec)'
+	title= 'Dynamic Programming Approach: Time Elapsed'
+	plotin3d(fig,sub,x,y,Z,xlab,ylab,title)
+	#  time vs L vs m, greedy
+	Z = mat[0,:,:]
+	sub  = '326'
+	xlab = 'Length of Genome'
+	ylab = 'Number of Forward CTCF Binding Motifs'
+	zlab = 'Time Elapsed (sec)'
+	title= 'Greedy Approach: Time Elapsed'
+	plotin3d(fig,sub,x,y,Z,xlab,ylab,title)
+
+
+	plt.show()
 
 
